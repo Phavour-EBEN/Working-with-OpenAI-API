b051de1779f09d02a342531f758fdf67618dea97
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-01-28 20:54:21 +0000

    first commit

---
+++ OpenAPI_API.py
@@ -0,0 +1,12 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv('API_KEY'))
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "system", "content": "You are a helpful assistant."}]
+)
+print(response)
