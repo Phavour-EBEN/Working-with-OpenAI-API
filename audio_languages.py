98ba43531eadc9ec7a59a22a7b3d005c7e2457aa
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-03-21 01:25:53 +0000

    Adds openai audio languages lesons

---
+++ audio_languages.py
@@ -0,0 +1,16 @@
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Open the audio.wav file
+audio_file = open("audio.wav", "rb")
+
+# Create a transcription request using audio_file
+audio_response = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+
+# Create a request to the API to identify the language spoken
+chat_response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{
+        "role":"user",
+        "content": audio_response.text}]
+)
+print(chat_response.choices[0].message.content)
